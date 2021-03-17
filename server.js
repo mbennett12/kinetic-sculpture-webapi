dc3bfe0b3b52c65e4f22d14c9d21fd275c8ae2cc
Maxwell Bennett <maxwellbennett@maxwells-mbp-2.lan>
2021-03-16 22:45:30 -0400

    server page for controlling all, mobile friendly

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
+var path = require('path');
 var methodOverride = require('method-override');
 var favicon = require('serve-favicon');
 const express = require('express')
@@ -18,12 +19,10 @@ app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
 
-
 app.get('/', (req, res) => {
   res.send('Go to \'/your-uni\' to find your button,\n' +
            'and \'/your-uni/running\' to query whether to run')
 })
-
 
 // Returns page per unique ID (uni) for each student in class
 app.get('/:id/', function (req, res) {
@@ -35,27 +34,31 @@ app.get('/:id/', function (req, res) {
   return res.sendFile(__dirname + '/id.html')
 })
 
-
 // Called from the button on {example-id}.html
 // causes {example-id}/running to return true for 30s
+// note: 'server' button turns all running files true for 30s
 app.put('/:id/', function (req, res) {
   var url_parts = url.parse(req.url);
   var id = url_parts.pathname.split('/')[1] // get id from pathname
 
-  var filename = __dirname + url_parts.pathname + "/running";
+  if (id == "server") {
+    turnAllOn();
+    return res.end()
+  } else {
+    var filename = path.join(__dirname, url_parts.pathname, "running");
+    turnOnFor30(filename);
+    console.log(filename + " --> true")
+    return res.end();
+  }
 
-  turnOffIn30(filename);
-  console.log(filename + " --> true")
-  return res.end();
 })
-
 
 // returns false by default, or true if button on {example-id} has 
 // has been recently pressed
 app.get('/:id/running', function (req, res) {
   var url_parts = url.parse(req.url);
   var id = url_parts.pathname.split('/')[1] // get id from pathname
-  var filename = __dirname + url_parts.pathname;
+  var filename = path.join(__dirname, url_parts.pathname);
 
   fs.existsSync(`${id}/`) || fs.mkdirSync(`${id}/`);
   
@@ -73,7 +76,7 @@ app.get('/:id/running', function (req, res) {
 })
 
 // writes {example-id}/running to true for 30s and then back to false
-function turnOffIn30(filename) {
+function turnOnFor30(filename) {
   var timeleft = 30;
   var runningTimer = setInterval(function(){
     if (timeleft <= 0){
@@ -89,4 +92,27 @@ function turnOffIn30(filename) {
     });
     timeleft -= 1;
   }, 1000);
+}
+
+// turns all 'running' files in directories to true for 30s
+function turnAllOn() {
+  console.log("**** Turning all 'running' files to true")
+  // get all directories
+  const getDirectories = source =>
+  fs.readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
+  var directories = getDirectories(__dirname);
+
+  // turn all 'running' files to true for 30s
+  directories.forEach(dir => {
+    fs.readdirSync(dir).forEach(file => {
+      if (file == "running") {
+        var filename = path.join(__dirname, dir, file);
+        console.log(filename + " --> true")
+        turnOnFor30(filename);
+      }
+    });
+  });
+  return
 }
